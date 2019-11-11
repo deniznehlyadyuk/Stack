5a7ed77bd92e32c36eef269fbb549208e296db9a
deniznehlyadyuk <deniznehlyadyuk@gmail.com>
2019-11-11 17:44:39 +0300

    Stack, tüm veri türleri için kullanılabilir hâle getirildi.

--- Code/inc/stack.h
+++ Code/inc/stack.h
@@ -1,22 +1,87 @@
 #ifndef STACK_H
 #define STACK_H
 
-class Node;
+template<typename T>
+class Node {
+public:
+	T m_data;
+	Node<T>* m_next;
+};
 
+template<typename T>
 class Stack {
 private:
 	int m_size;
-	Node* m_top;
+	Node<T>* m_top;
 
 public:
 	Stack();
 
-	void push(int data);
+	void push(T data);
 	void pop();
 
-	int top();
+	T top();
 	int size();
 	bool empty();
 };
+
+template<typename T>
+Stack<T>::Stack() {
+	m_top = nullptr;
+	m_size = 0;
+}
+
+template<typename T>
+void Stack<T>::push(T data) {
+	Node<T>* tmp = new Node<T>;
+
+	tmp->m_data = data;
+	tmp->m_next = m_top;
+	m_top = tmp;
+	m_size++;
+}
+
+template<typename T>
+void Stack<T>::pop() {
+	if (m_top == nullptr) {
+		std::cerr << "Stack already empty" << std::endl;
+	}
+	else {
+		if (--m_size == 0) {
+			m_top = nullptr;
+		}
+		else {
+			Node<T>* tmp = m_top->m_next;
+			delete m_top;
+			m_top = tmp;
+		}
+	}
+}
+
+template<typename T>
+T Stack<T>::top() {
+	if (m_top == nullptr) {
+		std::cerr << "Stack is empty" << std::endl;
+		return 0;
+	}
+	else {
+		return m_top->m_data;
+	}
+}
+
+template<typename T>
+int Stack<T>::size() {
+	return m_size;
+}
+
+template<typename T>
+bool Stack<T>::empty() {
+	if (m_top == nullptr) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
 
 #endif // !STACK_H
--- Test/main.cpp
+++ Test/main.cpp
@@ -3,38 +3,115 @@
 #include <ctime>
 #include "stack.h"
 
+std::string randomString() {
+	std::string src = "LAKSJDIVMVZQPEIRUTYEMCZ";
+	return src.substr(0, rand()%(src.length()-15)+1);
+}
+
 int main()
 {
 	srand(static_cast<unsigned int> (time(NULL)));
 
-	Stack stack;
-
-	std::cout << "Test: Pushing elements\t\t";
+	Stack<int> stack_i;
+	Stack<float> stack_f;
+	Stack<bool> stack_b;
+	Stack<char> stack_c;
+	Stack<std::string> stack_s;
+	
+	std::cout << "Test: Push element to stack\t\t(integer)\t";
 	for (int i = 0; i < rand() % 15 + 5; i++) {
-		stack.push(rand() % 20 + 5);
+		stack_i.push(rand() % 15 + 5);
 	}
 	std::cout << "Passed!" << std::endl;
 
-	std::cout << "Test: Access stack size\t\t" << stack.size() << std::endl;
+	std::cout << "Test: Access size of stack\t\t(integer)\t" << stack_i.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(integer)\t" << stack_i.empty() << std::endl;
 
-	std::cout << "Test: Access stack top\t\t" << stack.top() << std::endl;
-
-	std::cout << "Test: Poping element\t\t";
-	stack.pop();
-	std::cout << "Passed!" << std::endl;
-
-	std::cout << "Test: Access stack top\t\t" << stack.top() << std::endl;
-
-	std::cout << "Test: Is Empty\t\t\t" << stack.empty() << std::endl;
-
-	std::cout << "Test: Delete all elements\t";
-	while (!stack.empty()) {
-		std::cout << stack.top() << " ";
-		stack.pop();
+	std::cout << "Test: Access 'top' and delete 'top'\t(integer)\t";
+	while (!stack_i.empty()) {
+		std::cout << stack_i.top() << " ";
+		stack_i.pop();
 	}
 	std::cout << std::endl;
 
-	std::cout << "Test: Is Empty\t\t\t" << stack.empty() << std::endl;
+	std::cout << "Test: Access size of stack\t\t(integer)\t" << stack_i.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(integer)\t" << stack_i.empty() << std::endl << std::endl;
+
+	std::cout << "Test: Push element to stack\t\t(float)\t\t";
+	for (int i = 0; i < rand() % 15 + 5; i++) {
+		stack_f.push((rand() % 15 + 5)*0.7);
+	}
+	std::cout << "Passed!" << std::endl;
+
+	std::cout << "Test: Access size of stack\t\t(float)\t\t" << stack_f.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(float)\t\t" << stack_f.empty() << std::endl;
+
+	std::cout << "Test: Access 'top' and delete 'top'\t(float)\t\t";
+	while (!stack_f.empty()) {
+		std::cout << stack_f.top() << " ";
+		stack_f.pop();
+	}
+	std::cout << std::endl;
+
+	std::cout << "Test: Access size of stack\t\t(float)\t\t" << stack_f.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(float)\t\t" << stack_f.empty() << std::endl << std::endl;
+
+	std::cout << "Test: Push element to stack\t\t(bool)\t\t";
+	for (int i = 0; i < rand() % 15 + 5; i++) {
+		stack_b.push(rand()%2);
+	}
+	std::cout << "Passed!" << std::endl;
+
+	std::cout << "Test: Access size of stack\t\t(bool)\t\t" << stack_b.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(bool)\t\t" << stack_b.empty() << std::endl;
+
+	std::cout << "Test: Access 'top' and delete 'top'\t(bool)\t\t";
+	while (!stack_b.empty()) {
+		std::cout << stack_b.top() << " ";
+		stack_b.pop();
+	}
+	std::cout << std::endl;
+
+	std::cout << "Test: Access size of stack\t\t(bool)\t\t" << stack_b.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(bool)\t\t" << stack_b.empty() << std::endl << std::endl;
+
+	std::cout << "Test: Push element to stack\t\t(char)\t\t";
+	for (int i = 0; i < rand() % 15 + 5; i++) {
+		stack_c.push((char)(rand() % (127-33) + 33));
+	}
+	std::cout << "Passed!" << std::endl;
+
+	std::cout << "Test: Access size of stack\t\t(char)\t\t" << stack_c.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(char)\t\t" << stack_c.empty() << std::endl;
+
+	std::cout << "Test: Access 'top' and delete 'top'\t(char)\t\t";
+	while (!stack_c.empty()) {
+		std::cout << stack_c.top() << " ";
+		stack_c.pop();
+	}
+	std::cout << std::endl;
+
+	std::cout << "Test: Access size of stack\t\t(char)\t\t" << stack_c.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(char)\t\t" << stack_c.empty() << std::endl << std::endl;
+
+	std::cout << "Test: Push element to stack\t\t(string)\t";
+	for (int i = 0; i < rand() % 15 + 5; i++) {
+		stack_s.push(randomString());
+	}
+	std::cout << "Passed!" << std::endl;
+
+	std::cout << "Test: Access size of stack\t\t(string)\t" << stack_s.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(string)\t" << stack_s.empty() << std::endl;
+
+	std::cout << "Test: Access 'top' and delete 'top'\t(string)\t";
+	while (!stack_s.empty()) {
+		std::cout << stack_s.top() << " ";
+		stack_s.pop();
+	}
+	std::cout << std::endl;
+
+	std::cout << "Test: Access size of stack\t\t(string)\t" << stack_s.size() << std::endl;
+	std::cout << "Test: Is empty\t\t\t\t(string)\t" << stack_s.empty() << std::endl << std::endl;
 
 	system("pause");
 	return 0;
